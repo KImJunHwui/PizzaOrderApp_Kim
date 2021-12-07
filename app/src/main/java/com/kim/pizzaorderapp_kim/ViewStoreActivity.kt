2c41a3a072f1cce6bbf632bed3adc79b9720cfb6
KJH <daeding7@naver.com>
2021-12-07 21:08:13 +0900

    가게 상세보기 화면 추가

---
+++ app/src/main/java/com/kim/pizzaorderapp_kim/ViewStoreActivity.kt
@@ -0,0 +1,22 @@
+package com.kim.pizzaorderapp_kim
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewStoreActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_store)
+        setupEvents()
+        setValues()
+    }
+
+
+    override fun setupEvents() {
+    }
+
+    override fun setValues() {
+
+    }
+}
